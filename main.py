5422fb3cb83b6c0b1b962ce755c12454f2e4bc35
Soumya Ranjan Sahoo <srsahoo2001@gmail.com>
2022-04-09 23:41:31 +0530

    added changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://wrathgod01.github.io/sales-prediction-frontend/", "localhost:5500/index.html"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
